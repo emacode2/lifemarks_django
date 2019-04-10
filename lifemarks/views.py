2f077363ba8fa9f324043cac8d8e779d1a11bb2b
Emmanuel Asu <mannyasu2@gmail.com>
2019-04-10 15:31:38 -0400

    rendered mark list template, added mark detail in views.py

--- lifemarks/views.py
+++ lifemarks/views.py
@@ -7,3 +7,7 @@ from .models import Mark
 def mark_list(request):
     marks = Mark.objects.all()
     return render(request, 'lifemarks/mark_list.html', {'marks': marks})
+
+def mark_detail(request, pk):
+    mark = Mark.objects.get(id=pk)
+    return render(request, 'lifemarks/mark_detail.html', {'mark': mark})    
