36f7361d97a5d73347f4350c0d1f64211b7eae14
Emmanuel Asu <mannyasu2@gmail.com>
2019-04-10 10:15:36 -0400

    created models

--- lifemarks/models.py
+++ lifemarks/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Mark(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    photo_url = models.TextField()
+    category = models.CharField(max_length=100, default="category")
+
+    def __str__(self):
+        return self.title
